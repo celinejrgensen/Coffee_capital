5ae63a4fcd813d62693f81be6046f910efac0d08
celinejrgensen <celinejrgensen@gmail.com>
2016-10-06 01:55:00 +1100

    Add files via upload

---
+++ Well_known.js
@@ -0,0 +1,33 @@
+    function show_Starbucks_kart()
+    {
+    document.getElementById('Starbucks_kart').style.visibility="visible";
+    }
+
+    function hide_Starbucks_kart()
+    {
+    document.getElementById('Starbucks_kart').style.visibility="hidden";
+    }
+
+
+
+    function show_Gloria()
+    {
+    document.getElementById('Gloria').style.visibility="visible";
+    }
+
+    function hide_Gloria()
+    {
+    document.getElementById('Gloria').style.visibility="hidden";
+    }
+
+
+
+    function show_Hudson()
+    {
+    document.getElementById('Hudson').style.visibility="visible";
+    }
+
+    function hide_Gloria()
+    {
+    document.getElementById('Hudson').style.visibility="hidden";
+    }
