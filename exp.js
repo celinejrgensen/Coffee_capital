a97123cf560e52a377c86b81ebaf3d3841a70920
celinejrgensen <celinejrgensen@gmail.com>
2016-10-06 00:29:41 +1100

    Add files via upload

---
+++ exp.js
@@ -0,0 +1,79 @@
+/* Sound1 */
+
+function PlaySound(mySound1) {
+    var thissound=document.getElementById(mySound1);
+    thissound.play();
+}
+
+function StopSound(mySound1) {
+    var thissound=document.getElementById(mySound1);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+/* Sound2 */
+
+function PlaySound(mySound2) {
+    var thissound=document.getElementById(mySound2);
+    thissound.play();
+}
+
+function StopSound(mySound2) {
+    var thissound=document.getElementById(mySound2);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+/* Sound3 */
+
+function PlaySound(mySound3) {
+    var thissound=document.getElementById(mySound3);
+    thissound.play();
+}
+
+function StopSound(mySound3) {
+    var thissound=document.getElementById(mySound3);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+/* Sound4 */
+
+function PlaySound(mySound4) {
+    var thissound=document.getElementById(mySound4);
+    thissound.play();
+}
+
+function StopSound(mySound4) {
+    var thissound=document.getElementById(mySound4);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+/* Sound5 */
+
+function PlaySound(mySound5) {
+    var thissound=document.getElementById(mySound5);
+    thissound.play();
+}
+
+function StopSound(mySound5) {
+    var thissound=document.getElementById(mySound5);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+/* Sound6 */
+
+function PlaySound(mySound6) {
+    var thissound=document.getElementById(mySound6);
+    thissound.play();
+}
+
+function StopSound(mySound6) {
+    var thissound=document.getElementById(mySound6);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+/* Gifs */
